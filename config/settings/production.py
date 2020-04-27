00f695569eba985ee6920ae492a79051c41d37a3
Aquiles Carattino <aqui.carattino@gmail.com>
2020-04-27 08:00:27 +0200

    Updated e-mail domain

--- config/settings/production.py
+++ config/settings/production.py
@@ -103,7 +103,7 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="Aquiles Carattino Page <noreply@aquicarattino.com>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="Aquiles Carattino Page <noreply@mail.aquicarattino.com>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
