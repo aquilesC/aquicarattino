d41f9526a331209e3adf292ca298ab1e00d345b4
Aquiles Carattino <aqui.carattino@gmail.com>
2020-04-29 06:39:08 +0200

    Added wagtail style guide

--- config/settings/local.py
+++ config/settings/local.py
@@ -33,7 +33,7 @@ EMAIL_BACKEND = env(
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
+INSTALLED_APPS += ["wagtail.contrib.styleguide", "debug_toolbar"]  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
